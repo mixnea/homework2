11e900763231a2e994a6b74477a66812d018a026
mhnco <mhnco@DESKTOP-4SMJ4ST>
2026-03-29 19:17:29 +0300

    Add project files.

---
+++ homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static bool IsPalindrome(string s)
+    {
+        string normalized = s.ToLower().Replace(" ", "");//litera mici si scoate spatiile
+        char[] chars = normalized.ToCharArray();
+        Array.Reverse(chars);//de la coada la cap
+        string reversed = new string(chars);
+        return normalized == reversed;
+    }
+    static void Main()
+    {
+        Console.WriteLine(IsPalindrome("Ana"));                         
+        Console.WriteLine(IsPalindrome("A man a plan a canal Panama"));
+        Console.WriteLine(IsPalindrome("Hello"));                       
+    }
+}
